65af345e4ed8ef7c3cbbc44a5c80f312cc2a9f82
GengiuRobert <fncrobert3@gmail.com>
2025-03-15 18:11:28 +0200

    no more camel case paths in front end

--- frontend/gymhive/src/app/services/crudproducts.service.ts
+++ frontend/gymhive/src/app/services/crudproducts.service.ts
@@ -13,22 +13,22 @@ export class ProductService{
     constructor(private http: HttpClient){}
 
     getAllProducts(): Observable<Product[]>{
-        let my_url = this.baseUrl + "/getAllProducts";
+        let my_url = this.baseUrl + "/get-all-products";
         return this.http.get<Product[]>(my_url);
     }
 
     addProduct(product: Product): Observable<string>{
-        let my_url = this.baseUrl + "/addProduct";
+        let my_url = this.baseUrl + "/add-product";
         return this.http.post(`${my_url}`, product, {responseType : 'text'});
     }
 
     updateProduct(productId: string, product: Product): Observable<string> {
-        let my_url = this.baseUrl + "/updateProductById?productId=";
+        let my_url = this.baseUrl + "/update-product-by-id?productId=";
         return this.http.put(`${my_url}${productId}`,product, {responseType : 'text'});
     }
 
     deleteProduct(productId: string): Observable<string>{
-        let my_url = this.baseUrl + "/deleteProductById?productId=";
+        let my_url = this.baseUrl + "/delete-product-by-id?productId=";
         return this.http.delete(`${my_url}${productId}`, {responseType : 'text'});
     }
 }
