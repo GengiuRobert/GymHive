458e41e0d650f258f4a8c29cdc2fcec5f6c82db0
GengiuRobert <fncrobert3@gmail.com>
2025-03-27 22:51:52 +0200

    added category field on product

--- backend/GymHive/src/main/java/com/example/gymhive/entity/Product.java
+++ backend/GymHive/src/main/java/com/example/gymhive/entity/Product.java
@@ -15,5 +15,6 @@ public class Product {
     private String name;
     private String description;
     private Double price;
+    private String category;
 
 }
--- backend/GymHive/src/main/java/com/example/gymhive/entity/ShoppingCart.java
+++ backend/GymHive/src/main/java/com/example/gymhive/entity/ShoppingCart.java
@@ -1,4 +1,17 @@
 package com.example.gymhive.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class ShoppingCart {
+    private String shoppingCartId;
+
+
 }
